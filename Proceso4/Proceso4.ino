cf03c0aca774a4a1f77c5689b379ef7e1120a9bf
Oscar Daniel Reyes León <oscar@mercax.com>
2022-07-25 13:38:38 -0500

    Parar alimentador si molino esta apagado

--- Proceso4/Proceso4.ino
+++ Proceso4/Proceso4.ino
@@ -1,38 +1,50 @@
 #include <Controllino.h>
 
 float detenerAlimentador = 8.0/0.03;
+float prenderAlimentador = 0.5/0.03;
 int muestras = 16;
-int espera = 125;
+int espera = 63;
 
 void setup() {
   pinMode(CONTROLLINO_A3, INPUT);
   pinMode(CONTROLLINO_D2, OUTPUT);
-  pinMode(CONTROLLINO_D3, OUTPUT);
   pinMode(CONTROLLINO_D6, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop() {
   float cargaPromedio = calculoMotor();
-
-  if (cargaPromedio <= detenerAlimentador){
-    digitalWrite(CONTROLLINO_D2, 1);
-    digitalWrite(CONTROLLINO_D3, 1);
-    digitalWrite(CONTROLLINO_D6, 0);
-    delay(500);
-    digitalWrite(CONTROLLINO_D6, 1);
-  }
-  else{
+  if (cargaPromedio > detenerAlimentador){
     digitalWrite(CONTROLLINO_D2, 0);
-    digitalWrite(CONTROLLINO_D3, 0);
-    for(int i=0;i<20;i++)
+    for(int i=0;i<12;i++)
     {
       digitalWrite(CONTROLLINO_D6, 1);
       delay(50);
       digitalWrite(CONTROLLINO_D6, 0);
       delay(50);
+      digitalWrite(CONTROLLINO_D6, 1);
+      delay(50);
+      digitalWrite(CONTROLLINO_D6, 0);
+      delay(100);
     }
+  } else if(cargaPromedio <= detenerAlimentador & cargaPromedio >= prenderAlimentador){
+    digitalWrite(CONTROLLINO_D2, 1);
+    digitalWrite(CONTROLLINO_D6, 1);
+  } else if(cargaPromedio < prenderAlimentador){
+    digitalWrite(CONTROLLINO_D2, 0);
+    for(int i=0;i<60;i++)
+    {
+      digitalWrite(CONTROLLINO_D6, 1);
+      delay(5000);
+      digitalWrite(CONTROLLINO_D6, 0);
+      delay(5000);
+    } 
+  } else {
+    digitalWrite(CONTROLLINO_D2, 0);
+    digitalWrite(CONTROLLINO_D6, 0);
   }
+
+  
 }
 
 float calculoMotor()
